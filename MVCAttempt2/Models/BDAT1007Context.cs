32800d56ad1e811587b9ae09dcf2af2d9c36205f
robertroutledge <58818711+robertroutledge@users.noreply.github.com>
2020-06-08 12:24:21 -0400

    Fixed missing files

---
+++ MVCAttempt2/Models/BDAT1007Context.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MVCAttempt2.Models
+{
+    public partial class BDAT1007Context : DbContext
+    {
+        public BDAT1007Context()
+        {
+        }
+
+        public BDAT1007Context(DbContextOptions<BDAT1007Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TicketOptions> TicketOptions { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("SERVER=DESKTOP-TVDSVH6;Database=BDAT1007;Integrated Security=True");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TicketOptions>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Category).HasMaxLength(50);
+
+                entity.Property(e => e.Esource)
+                    .HasColumnName("ESource")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Eurl)
+                    .HasColumnName("EURL")
+                    .HasMaxLength(150);
+
+                entity.Property(e => e.League).HasMaxLength(50);
+
+                entity.Property(e => e.TeamName).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ MVCAttempt2/Models/TicketOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCAttempt2.Models
+{
+    public partial class TicketOptions
+    {
+        public string Category { get; set; }
+        public string TeamName { get; set; }
+        public string Eurl { get; set; }
+        public string League { get; set; }
+        public string Esource { get; set; }
+        public int Id { get; set; }
+    }
+}
